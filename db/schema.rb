6c6333290367342135a213116eacf643e25fadeb
Francisco <fcontreras18@gmail.com>
2016-10-20 06:09:17 +0000

    Student course page

--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,13 @@ ActiveRecord::Schema.define(version: 20161017194123) do
   enable_extension "plpgsql"
 
   create_table "courses", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "title"
+    t.text     "description"
+    t.decimal  "cost"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["user_id"], name: "index_courses_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
